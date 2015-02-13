268fb3b228b855c547d2048314ae6d54035c918c
Ricky Plum <rickyplum@gmail.com>
2015-02-12 23:24:12 -0500

    Added an overlay to the collection view

--- app/scripts/collection.js
+++ app/scripts/collection.js
@@ -1,3 +1,51 @@
+// use a loop to create an array of objects
+// for every item in the array, iterate over its properties and write the key/value pairs
+/*
+  [{
+    username: "rickyplum",
+    firstname: "Ricky",
+    lastname: "Plum"
+  }]
+
+  // "username = rickyplum"
+  // "firstname = Ricky"
+  // "lastname = Plum" 
+
+
+
+*/
+
+window.exercise = function () {
+  console.log("For loop exercise");
+
+  // create an array of users
+  var users = [];
+
+  for (var i = 0; i < 50; i++) {
+    console.log("Creating user #" + i);
+    //debugger;
+
+    users.push({
+      username: "user" + i,
+      firstname: "first name" + i,
+      lastname: "last name" + i
+    });
+  }
+
+  // for every user, write out all user properties and values.
+  // create a function for writing out all user properties and call it in your for loop
+  for (var i = 0; i < users.length; i++) {
+    showUser(users[i]);
+  }
+};
+
+var showUser = function(user) {
+  console.log('username: ' + user.username);
+  console.log('first name: ' + user.firstname);
+  console.log('last name: ' + user.lastname);
+}
+
+
  var updateCollectionView = function() {
    var $collection = $(".collection-container .row");
    $collection.empty();
@@ -6,13 +54,27 @@
      var $newThumbnail = buildAlbumThumbnail();
      $collection.append($newThumbnail);
    }
+
+      var onHover = function(event) {
+     $(this).append(buildAlbumOverlay("/album.html"));
+   };
+
+   var offHover = function(event) {
+    $(this).find('.collection-album-image-overlay').remove();
+  };
+
+   $collection.find('.collection-album-image-container').hover(onHover, offHover);
+
+   $collection.find('.collection-album-image-container').hover(onHover);
  };
  
 
  var buildAlbumThumbnail = function() {
     var template =
         '<div class="collection-album-container col-md-2">'
-      + '  <img src="/images/album-placeholder.png"/>'
+      + '    <div class="collection-album-image-container">'
+      + '    <img src="/images/album-placeholder.png"/>'
+      + '  </div>'
       + '  <div class="caption album-collection-info">'
       + '    <p>'
       + '      <a class="album-name" href="/album.html"> Album Name </a>'
@@ -35,3 +97,20 @@
      updateCollectionView();
    });
  }
+
+ var buildAlbumOverlay = function(albumURL) {
+    var template =
+        '<div class="collection-album-image-overlay">'
+      + '  <div class="collection-overlay-content">'
+      + '    <a class="collection-overlay-button" href="' + albumURL + '">'
+      + '      <i class="fa fa-play"></i>'
+      + '    </a>'
+      + '    &nbsp;'
+      + '    <a class="collection-overlay-button">'
+      + '      <i class="fa fa-plus"></i>'
+      + '    </a>'
+      + '  </div>'
+      + '</div>'
+      ;
+    return $(template);
+  };
