db078daff1240da30fb83e4c9d0b9a0c0b0e5855
Ricky Plum <rickyplum@gmail.com>
2015-03-07 13:26:20 -0500

    merge mess up

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -273,6 +273,7 @@ require.register("scripts/album", function(exports, require, module) {
 require('./landing');
 	require('./collection');
 	require('./album');
+	require("./profile");
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -411,6 +412,28 @@ $(document).ready(function() {
  
    $('.selling-points .point').hover(onHoverAction, offHoverAction);
 });
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+ // holds the name of our tab button container for selection later in the function
+ var tabsContainer = ".user-profile-tabs-container"
+ var selectTabHandler = function(event) {
+   $tab = $(this);
+   $(tabsContainer + " li").removeClass('active');
+   $tab.parent().addClass('active');
+   selectedTabName = $tab.attr('href');
+   console.log(selectedTabName);
+   $(".tab-pane").addClass('hidden');
+   $(selectedTabName).removeClass('hidden');
+   event.preventDefault();
+ };
+  if (document.URL.match(/\/profile.html/)) {
+   $(document).ready(function() {
+     var $tabs = $(tabsContainer + " a");
+     $tabs.click(selectTabHandler);
+     $tabs[0].click();
+   });
+ }
 });
 
 ;
