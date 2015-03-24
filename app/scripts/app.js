3209b650feae3c30e6eb7a8962db33f01aa91790
Ricky Plum <rickyplum@gmail.com>
2015-03-24 01:32:56 -0400

    merge mess up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -126,6 +126,14 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
   blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.songPlayer = SongPlayer;
 
+  $scope.volumeClass = function() {
+     return {
+       'fa-volume-off': SongPlayer.volume == 0,
+       'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+       'fa-volume-up': SongPlayer.volume > 70
+     }
+   }
+
    SongPlayer.onTimeUpdate(function(event, time){
      $scope.$apply(function(){
        $scope.playTime = time;
@@ -144,6 +152,7 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
      currentSong: null,
      currentAlbum: null,
      playing: false,
+     volume: 90,
  
      play: function() {
        this.playing = true;
@@ -186,6 +195,13 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
       return $rootScope.$on('sound:timeupdate', callback);
     },
 
+    setVolume: function(volume) {
+      if(currentSoundFile){
+        currentSoundFile.setVolume(volume);
+      }
+      this.volume = volume;
+    },
+
      setSong: function(album, song) {
        if (currentSoundFile) {
       currentSoundFile.stop();
@@ -197,6 +213,8 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
       formats: [ "mp3" ],
       preload: true
     });
+
+       currentSoundFile.setVolume(this.volume);
 
         currentSoundFile.bind('timeupdate', function(e){
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
